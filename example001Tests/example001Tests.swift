8f0af06fe6c0c575325205753ad45a7855fa4c06
danielferrer <danybarco@gmail.com>
2020-07-16 07:15:40 +0200

    Initial Commit

---
+++ example001Tests/example001Tests.swift
@@ -0,0 +1,34 @@
+//
+//  example001Tests.swift
+//  example001Tests
+//
+//  Created by Daniel Ferrer on 16/07/2020.
+//  Copyright © 2020 3pies. All rights reserved.
+//
+
+import XCTest
+@testable import example001
+
+class example001Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
